57ce6a4df4c076ccc9fd9adcaee980cdbe499bfb
Bernardo Vieira <bernardo5@live.com.pt>
2020-06-03 21:03:17 +0100

    new tests

--- test/ImpactMarket.test.ts
+++ test/ImpactMarket.test.ts
@@ -2,7 +2,7 @@ import { should } from 'chai';
 import { ImpactMarketInstance, CommunityInstance, cUSDInstance } from '../types/truffle-contracts';
 import BigNumber from 'bignumber.js';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { expectRevert, time } = require('@openzeppelin/test-helpers');
+const { expectRevert, expectEvent, time } = require('@openzeppelin/test-helpers');
 
 
 const ImpactMarket = artifacts.require('./ImpactMarket.sol') as Truffle.Contract<ImpactMarketInstance>;
@@ -20,16 +20,17 @@ enum BeneficiaryState {
 BigNumber.config({ EXPONENTIAL_AT: 25 })
 /** @test {ImpactMarket} contract */
 contract('ImpactMarket', async (accounts) => {
-    const adminAccount = accounts[0];
+    const adminAccount1 = accounts[0];
+    const adminAccount2 = accounts[1];
     // community managers
-    const communityManagerA = accounts[1];
-    const communityManagerB = accounts[2];
-    const communityManagerC = accounts[3];
+    const communityManagerA = accounts[2];
+    const communityManagerB = accounts[3];
+    const communityManagerC = accounts[4];
     // beneficiaries
-    const beneficiaryA = accounts[4];
-    const beneficiaryB = accounts[5];
-    const beneficiaryC = accounts[6];
-    const beneficiaryD = accounts[7];
+    const beneficiaryA = accounts[5];
+    const beneficiaryB = accounts[6];
+    const beneficiaryC = accounts[7];
+    const beneficiaryD = accounts[8];
     // contract instances
     let impactMarketInstance: ImpactMarketInstance;
     let communityInstance: CommunityInstance;
@@ -38,7 +39,7 @@ contract('ImpactMarket', async (accounts) => {
     const decimals = new BigNumber(10).pow(18);
     const hour = time.duration.hours(1);
     const day = time.duration.days(1);
-    // const week = time.duration.weeks(1);
+    const week = time.duration.weeks(1);
     // const month = time.duration.days(30);
     const claimAmountTwo = new BigNumber('2').multipliedBy(decimals);
     const maxClaimTen = new BigNumber('10').multipliedBy(decimals);
@@ -53,7 +54,7 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             const communityManagerAddress = tx.logs[1].args[0];
             communityInstance = await Community.at(communityManagerAddress);
@@ -120,11 +121,11 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             const communityManagerAddress = tx.logs[1].args[0];
             communityInstance = await Community.at(communityManagerAddress);
-            await cUSDInstance.testFakeFundAddress(communityManagerAddress, { from: adminAccount });
+            await cUSDInstance.testFakeFundAddress(communityManagerAddress, { from: adminAccount1 });
             await communityInstance.addBeneficiary(beneficiaryA, { from: communityManagerA });
         });
 
@@ -148,6 +149,17 @@ contract('ImpactMarket', async (accounts) => {
             await expectRevert(
                 communityInstance.claim({ from: beneficiaryA }),
                 "REMOVED"
+            );
+        });
+
+        it('should not claim if community is locked', async () => {
+            const receipt = await communityInstance.lock({ from: communityManagerA });
+            expectEvent(receipt, 'CommunityLocked', {
+                _by: communityManagerA,
+            });
+            await expectRevert(
+                communityInstance.claim({ from: beneficiaryA }),
+                "LOCKED"
             );
         });
 
@@ -205,10 +217,73 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             const communityManagerAddress = tx.logs[1].args[0];
             communityInstance = await Community.at(communityManagerAddress);
+        });
+
+        it('should be able to migrate funds from community if coordinator', async () => {
+            const newTx = await impactMarketInstance.addCommunity(
+                communityManagerA,
+                claimAmountTwo,
+                day,
+                hour,
+                maxClaimTen,
+                { from: adminAccount1 },
+            );
+            const newCommunityAddress = newTx.logs[1].args[0];
+            await communityInstance.migrateFunds(newCommunityAddress, { from: communityManagerA });
+        });
+
+        it('should be able edit community if coordinator', async () => {
+            (await communityInstance.incIntervalTime()).toString().should.be.equal(hour.toString());
+            await communityInstance.edit(
+                claimAmountTwo,
+                week,
+                day,
+                maxClaimTen,
+                cUSDInstance.address,
+                { from: communityManagerA }
+            );
+            (await communityInstance.incIntervalTime()).toString().should.be.equal(day.toString());
+        });
+
+        it('should not be able edit community if not coordinator', async () => {
+            await expectRevert(
+                communityInstance.edit(
+                    claimAmountTwo,
+                    day,
+                    day,
+                    maxClaimTen,
+                    cUSDInstance.address,
+                    { from: communityManagerB }
+                ),
+                "NOT_COORDINATOR"
+            );
+        });
+
+        it('should not be able edit community with invalid values', async () => {
+            await expectRevert.unspecified(
+                communityInstance.edit(
+                    claimAmountTwo,
+                    day,
+                    week,
+                    maxClaimTen,
+                    cUSDInstance.address,
+                    { from: communityManagerA }
+                )
+            );
+            await expectRevert.unspecified(
+                communityInstance.edit(
+                    maxClaimTen, // supposed to be wrong
+                    week,
+                    day,
+                    claimAmountTwo,
+                    cUSDInstance.address,
+                    { from: communityManagerA }
+                )
+            );
         });
 
         it('should not be able to add coordinator to community if not coordinator', async () => {
@@ -234,6 +309,32 @@ contract('ImpactMarket', async (accounts) => {
             await communityInstance.addCoordinator(communityManagerB, { from: communityManagerA });
             await communityInstance.removeCoordinator(communityManagerB, { from: communityManagerA });
         });
+
+        it('should be able to renounce from coordinator of community if coordinator', async () => {
+            await communityInstance.addCoordinator(communityManagerB, { from: communityManagerA });
+            await communityInstance.renounceRole(
+                await communityInstance.COORDINATOR_ROLE(),
+                communityManagerB, { from: communityManagerB }
+            );
+        });
+
+        it('should be able to lock community if coordinator', async () => {
+            const receipt = await communityInstance.lock({ from: communityManagerA });
+            expectEvent(receipt, 'CommunityLocked', {
+                _by: communityManagerA,
+            });
+        });
+
+        it('should be able to lock community if coordinator', async () => {
+            let receipt = await communityInstance.lock({ from: communityManagerA });
+            expectEvent(receipt, 'CommunityLocked', {
+                _by: communityManagerA,
+            });
+            receipt = await communityInstance.unlock({ from: communityManagerA });
+            expectEvent(receipt, 'CommunityUnlocked', {
+                _by: communityManagerA,
+            });
+        });
     });
 
     describe('ImpactMarket', () => {
@@ -249,7 +350,7 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             const communityManagerAddress = tx.logs[1].args[0];
             communityInstance = await Community.at(communityManagerAddress);
@@ -271,10 +372,59 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             const communityManagerAddress = tx.logs[1].args[0];
-            await impactMarketInstance.removeCommunity(communityManagerAddress, { from: adminAccount });
+            await impactMarketInstance.removeCommunity(communityManagerAddress, { from: adminAccount1 });
+        });
+
+        it('should be able to add admin', async () => {
+            const receipt = await impactMarketInstance.addAdmin(adminAccount2, { from: adminAccount1 });
+            expectEvent(receipt, 'RoleGranted', {
+                role: await impactMarketInstance.ADMIN_ROLE(),
+                account: adminAccount2,
+                sender: adminAccount1,
+            });
+        });
+
+        it('should be able to remove admin', async () => {
+            await impactMarketInstance.addAdmin(adminAccount2, { from: adminAccount1 });
+            const receipt = await impactMarketInstance.removeAdmin(adminAccount2, { from: adminAccount1 });
+            expectEvent(receipt, 'RoleRevoked', {
+                role: await impactMarketInstance.ADMIN_ROLE(),
+                account: adminAccount2,
+                sender: adminAccount1,
+            });
+        });
+
+        it('should not be able to add admin if not admin', async () => {
+            await expectRevert(
+                impactMarketInstance.addAdmin(adminAccount2, { from: adminAccount2 }),
+                'NOT_ADMIN'
+            );
+        });
+
+        it('should not be able to create a community with invalid values', async () => {
+            await expectRevert.unspecified(
+                impactMarketInstance.addCommunity(
+                    communityManagerA,
+                    claimAmountTwo,
+                    hour,
+                    day,
+                    maxClaimTen,
+                    { from: adminAccount1 },
+                )
+            );
+            await expectRevert.unspecified(
+                impactMarketInstance.addCommunity(
+                    communityManagerA,
+                    maxClaimTen, // it's supposed to be wrong!
+                    day,
+                    hour,
+                    claimAmountTwo,
+                    { from: adminAccount1 },
+                )
+            );
         });
     });
 
@@ -287,12 +437,12 @@ contract('ImpactMarket', async (accounts) => {
                 day,
                 hour,
                 maxClaimTen,
-                { from: adminAccount },
+                { from: adminAccount1 },
             );
             // eslint-disable-next-line prefer-const
             const communityManagerAddress = tx.logs[1].args[0] as string;
             const instance = await Community.at(communityManagerAddress);
-            await cUSDInstance.testFakeFundAddress(communityManagerAddress, { from: adminAccount });
+            await cUSDInstance.testFakeFundAddress(communityManagerAddress, { from: adminAccount1 });
             return instance;
         }
         // add beneficiary
