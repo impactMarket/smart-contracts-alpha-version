d6cc3f5bbe08bb17139a8aaf71f7568d5f5c26b6
Bernardo Vieira <bernardo5@live.com.pt>
2020-08-03 16:31:38 +0100

    update testnet addresses

--- migrations/2_deploy_impactmarket.js
+++ migrations/2_deploy_impactmarket.js
@@ -1,16 +1,15 @@
+require('dotenv').config({ path: '../.env' })
 const ImpactMarket = artifacts.require('ImpactMarket');
 const CommunityFactory = artifacts.require('CommunityFactory');
 
+
 module.exports = async (deployer, network, accounts) => {
     if (network === 'alfajores') {
-        const cUSDAddress = '0x874069fa1eb16d44d622f2e0ca25eea172369bc1';
+        const cUSDAddress = process.env.CUSD_ALFAJORES_ADDRESS;
         await deployer.deploy(ImpactMarket, cUSDAddress, [
-            // afonso address
-            '0xd667ff2728475b298e28fe3c4a9ca2ad4fc162f1',
-            // marco address
-            '0x1ebd10f45daad2ed949cfd0e60ff36c3c941a556',
-            // bernardo address
-            '0x60f2b1ee6322b3aa2c88f497d87f65a15593f452',
+            process.env.MARCO_STAGING_WALLET_ADDRESS,
+            process.env.AFONSO_STAGING_WALLET_ADDRESS,
+            process.env.BERNARDO_STAGING_WALLET_ADDRESS,
         ]);
         const impactMarket = await ImpactMarket.deployed();
         await deployer.deploy(CommunityFactory, cUSDAddress, impactMarket.address);
