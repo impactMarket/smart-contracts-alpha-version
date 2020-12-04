6dccb8ff0adb856a082a5e2811e9a404af603220
Bernardo Vieira <bernardo5@live.com.pt>
2020-12-04 20:30:22 +0000

    migrate beneficiary test

--- test/ubi/community/Beneficiary.test.ts
+++ test/ubi/community/Beneficiary.test.ts
@@ -1,12 +1,14 @@
+import { ethers } from "hardhat";
 import { should } from 'chai';
+import { Contract } from 'ethers';
 
-import {
-    ImpactMarketInstance,
-    CommunityInstance,
-    CUSDInstance,
-    CommunityFactoryInstance,
-} from '../../../types/truffle-contracts';
-import { defineAccounts } from '../../helpers/accounts';
+// import {
+//     ImpactMarketInstance,
+//     CommunityInstance,
+//     CUSDInstance,
+//     CommunityFactoryInstance,
+// } from '../../../types/truffle-contracts';
+import { AccountsAddress, AccountsSigner, defineAccounts, defineSigners } from '../../helpers/accounts';
 import {
     hour,
     day,
@@ -14,166 +16,152 @@ import {
     maxClaimTen,
     fiveCents,
 } from '../../helpers/constants';
-import {
-    ImpactMarket,
-    Community,
-    CommunityFactory,
-    cUSD,
-} from '../../helpers/contracts';
+// import {
+//     ImpactMarket,
+//     Community,
+//     CommunityFactory,
+//     cUSD,
+// } from '../../helpers/contracts';
 import { BeneficiaryState } from '../../helpers/utils';
+import { ImpactMarket } from "../../../types/ImpactMarket";
+import { CUSD } from "../../../types/CUSD";
+import { Community } from "../../../types/Community";
+import { CommunityFactory } from "../../../types/CommunityFactory";
+
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { expectRevert } = require('@openzeppelin/test-helpers');
 should();
 
 /** @test {Community} contract */
-contract('Community - Beneficiary', async (accounts) => {
-    const { adminAccount1, communityManagerA, beneficiaryA } = defineAccounts(
-        accounts
-    );
+describe('Community - Beneficiary', () => {
+    let accounts: AccountsAddress;
+    let signers: AccountsSigner;
     // contract instances
-    let impactMarketInstance: ImpactMarketInstance;
-    let communityInstance: CommunityInstance;
-    let communityFactoryInstance: CommunityFactoryInstance;
-    let cUSDInstance: CUSDInstance;
+    let impactMarketInstance: Contract & ImpactMarket;
+    let communityInstance: Contract & Community;
+    let communityFactoryInstance: Contract & CommunityFactory;
+    let cUSDInstance: Contract & CUSD;
 
     beforeEach(async () => {
-        cUSDInstance = await cUSD.new();
-        impactMarketInstance = await ImpactMarket.new(cUSDInstance.address, [
-            adminAccount1,
-        ]);
-        communityFactoryInstance = await CommunityFactory.new(
+        accounts = await defineAccounts();
+        signers = await defineSigners();
+        //
+        const ImpactMarket = await ethers.getContractFactory("ImpactMarket");
+        const CommunityFactory = await ethers.getContractFactory("CommunityFactory");
+        const Community = await ethers.getContractFactory("Community");
+        const cUSD = await ethers.getContractFactory("cUSD");
+        cUSDInstance = await cUSD.deploy() as Contract & CUSD;
+        impactMarketInstance = await ImpactMarket.deploy(cUSDInstance.address, [
+            accounts.adminAccount1,
+        ]) as Contract & ImpactMarket;
+        communityFactoryInstance = await CommunityFactory.deploy(
             cUSDInstance.address,
             impactMarketInstance.address
-        );
+        ) as Contract & CommunityFactory;
         await impactMarketInstance.setCommunityFactory(
             communityFactoryInstance.address
         );
-        const tx = await impactMarketInstance.addCommunity(
-            communityManagerA,
+        const pendingTx = await impactMarketInstance.addCommunity(
+            accounts.communityManagerA,
             claimAmountTwo.toString(),
             maxClaimTen.toString(),
-            day,
-            hour,
-            { from: adminAccount1 }
+            day.toString(),
+            hour.toString()
         );
-        const communityManagerAddress = tx.logs[2].args[0];
-        communityInstance = await Community.at(communityManagerAddress);
-        await cUSDInstance.testFakeFundAddress(communityManagerAddress, {
-            from: adminAccount1,
-        });
+        const tx = await pendingTx.wait();
+        const communityAddress = tx.events![3].args![0];
+        communityInstance = await Community.attach(communityAddress) as Contract & Community;
+        communityInstance = communityInstance.connect(signers.communityManagerA) as Contract & Community;
+        await cUSDInstance.testFakeFundAddress(communityAddress);
     });
 
     it('should be able to add beneficiary to community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
     });
 
     it('should give beneficiary 5 cents when adding to community', async () => {
-        (await cUSDInstance.balanceOf(beneficiaryA))
+        (await cUSDInstance.balanceOf(accounts.beneficiaryA))
             .toString()
             .should.be.equal('0');
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await cUSDInstance.balanceOf(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await cUSDInstance.balanceOf(accounts.beneficiaryA))
             .toString()
             .should.be.equal(fiveCents.toString());
     });
 
     it('should be able to lock beneficiary from community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
-        await communityInstance.lockBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.lockBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Locked);
     });
 
     it('should not be able to lock an invalid beneficiary from community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await expectRevert(
-            communityInstance.lockBeneficiary(beneficiaryA, {
-                from: communityManagerA,
-            }),
-            'NOT_YET'
+        await expectRevert.unspecified(
+            communityInstance.lockBeneficiary(accounts.beneficiaryA),
+            // 'NOT_YET'
         );
     });
 
     it('should be able to unlock locked beneficiary from community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
-        await communityInstance.lockBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.lockBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Locked);
-        await communityInstance.unlockBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.unlockBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
     });
 
     it('should not be able to unlock a not locked beneficiary from community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
-        await expectRevert(
-            communityInstance.unlockBeneficiary(beneficiaryA, {
-                from: communityManagerA,
-            }),
-            'NOT_YET'
+        await expectRevert.unspecified(
+            communityInstance.unlockBeneficiary(accounts.beneficiaryA),
+            // 'NOT_YET'
         );
     });
 
     it('should be able to remove beneficiary from community', async () => {
-        (await communityInstance.beneficiaries(beneficiaryA))
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.NONE);
-        await communityInstance.addBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.addBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Valid);
-        await communityInstance.removeBeneficiary(beneficiaryA, {
-            from: communityManagerA,
-        });
-        (await communityInstance.beneficiaries(beneficiaryA))
+        await communityInstance.removeBeneficiary(accounts.beneficiaryA);
+        (await communityInstance.beneficiaries(accounts.beneficiaryA))
             .toString()
             .should.be.equal(BeneficiaryState.Removed);
     });
